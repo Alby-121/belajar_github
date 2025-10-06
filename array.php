f3518d8227d10e7741d3709f41f53faad0073473
Alby-121 <irmayantiasean@gmail.com>
2025-10-06 20:35:16 +0700

    menambahkahn file array php

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php
+// array
+// variabel yang dapat memiliki banyak nilai
+// elemen pada array boleh memiliki tipe data yang berbeda 
+// membuat array
+// cara lama
+$hari = array("Senin", "Selasa", "Rabu" );
+print_r($hari);
+    
